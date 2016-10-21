b4c12b916705bc7819e254bfb8744bd7bb79e7f8
Igor <igorrybalko2009@yandex.ru>
2016-10-21 22:44:28 +0300

    Create simpletabs.js

---
+++ simpletabs.js
@@ -0,0 +1,31 @@
+ //плагин табов
+(function($){
+
+    $.fn.simpleTabs = function (options) {
+
+        var settings = $.extend({
+            'title' : '.nametab', 
+            'content': '.contenttab',
+            'cb': ''
+        }, options);
+
+        var nametab = $(this).find(settings.title), /*селектор имен табов*/
+            contenttab = $(this).find(settings.content),/*селектор содержимого табов*/
+            callback = settings.cb,
+            tabsBlock = this;
+        nametab.on('click', function () {
+            var activeClass = $(this).hasClass('actheadtab');/*является ли имя таба активным?*/
+            if (!activeClass) {
+                var ind = $(this).index();
+                $(tabsBlock).find('.actheadtab').removeClass('actheadtab');
+                $(this).addClass('actheadtab');
+                $(tabsBlock).find('.activetab').removeClass('activetab');
+                contenttab.eq(ind).addClass('activetab');
+                if (callback) {
+                    callback();
+                }
+            }
+        });
+    };
+
+}(jQuery);
